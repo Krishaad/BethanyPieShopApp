5963fed4bd5bd47eb7088ea23c3d178942783794
Krishaad <xxk150@student.bham.ac.uk>
2024-07-31 16:53:29 +0100

    Basically completed Bethany shop HR manager

--- BethanyPieShop/Program.cs
+++ BethanyPieShop/Program.cs
@@ -4,19 +4,217 @@ namespace BethanyPieShop
 {
     class Program
     {
+        private static List<Employee> employeeList = new List<Employee>();
+
         static void Main(string[] args)
         {
-            Employee bethany = new Employee("Bethany", "Smith", "Bethany@gmail.com", new DateTime(1979, 1, 16),
-                EmployeeType.Manager, 25.2);
 
-            bethany.DisplayEmployeeDetails();
-            bethany.PerformWork();
-            bethany.PerformWork();
-            bethany.PerformWork();
-            bethany.PerformWork();
-            bethany.ReceiveWage();
+
+            bool checker = false;
+            do
+            {
+                Console.WriteLine("***********************************");
+                Console.WriteLine("Welcome to the Employee HR Manager!");
+                Console.WriteLine("1. Register Employee");
+                Console.WriteLine("2. Pay Employee");
+                Console.WriteLine("3. List Employee");
+                Console.WriteLine("4. Quit Application");
+                Console.WriteLine("***********************************");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                       // Console.WriteLine("Welcome to Register employee\n\n");
+                        //checker = true;
+                        RegisterEmployee();
+                        break;
+                    case "2":
+                        Console.WriteLine("***Welcome to payroll***\n\n");
+                        PayRoll();
+                        //checker = true;
+                        break;
+                    case "3":
+                        //Console.WriteLine("Welcome to Employees list\n\n");
+                        ListEmployees();
+                        break;
+                    case "4":
+                        Console.WriteLine("Are you sure you want to exit");
+                        checker = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter valid input\n");
+                        // checker = false;
+                        break;
+                }
+            } while (checker == false);
+
+        }
+
+        private static void RegisterEmployee()
+        {
+            string fname;
+            string lname;
+            double hourlyRate;
+
+            Console.WriteLine("Please enter employee's first name");
+            fname = Console.ReadLine();
+
+            Console.WriteLine($"Please enter {fname}'s last name");
+            lname = Console.ReadLine();
+
+            Console.WriteLine($"Please enter {fname} {lname}'s hourly rate");
+
+            while (!double.TryParse(Console.ReadLine(), out hourlyRate))
+            {
+                Console.WriteLine("Invalid input. Try again");
+            }
+            
+            //Employee employee = new Employee(fname, lname, hourlyRate);
+            employeeList.Add(new Employee(fname, lname, hourlyRate));
+
+            //Console.WriteLine(employeeList[0].FirstName);
+            Console.WriteLine($"Successfully added employee {fname} {lname}\n\n");
+
+        }
+
+
+        private static void ListEmployees()
+        {
+            if (employeeList.Count == 0)
+            {
+                Console.WriteLine("No employees yet");
+            }
+            else
+            {
+                for (int i = 0; i < employeeList.Count(); i++)
+                {
+                    Console.WriteLine($"{i+1}. {employeeList[i].FirstName}  {employeeList[i].LastName}");
+                    Console.WriteLine($"Hourly rate: {employeeList[i].Rate}\n\n");
+                }
+            }
+            
+        }
+
+        private static void PayRoll()
+        {
+            Console.WriteLine("Pick employee to pay now");
+            ListEmployees();
+
+            int lengthOfList = employeeList.Count;
+
+            int numPicked;
+
+            while (!int.TryParse(Console.ReadLine(), out numPicked))
+            {
+                Console.WriteLine("Enter valid number");
+            }
+
+            int positinInArr = numPicked - 1;
+
+            Console.WriteLine($"You picked {employeeList[positinInArr].FirstName}.\n{employeeList[positinInArr].FirstName}'s hourly rate is {employeeList[positinInArr].Rate}");
+            Console.WriteLine("Please enter how many hours worked");
+            int hoursWorked;
+
+            while (!int.TryParse(Console.ReadLine(), out hoursWorked))
+            {
+                Console.WriteLine("Enter valid number");
+            }
+
+            double currentWage = hoursWorked * employeeList[positinInArr].Rate;
+
+            Console.WriteLine($"{employeeList[positinInArr].FirstName}'s weekly wage is {currentWage}");
+
 
         }
     }
+    public class Employee
+    {
+
+
+
+        public Employee(string firstName, string lastName, double rate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Rate = rate;
+        }
+
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public double Rate
+        {
+            get;
+            set;
+        }
+
+
+
+        private string firstName;
+        private string lastName;
+        private double hourlyRate;
+        private double wage;
+
+        public string email;
+
+        public double numOfHoursWorked;
+
+
+
+        public DateTime bday;
+
+        public EmployeeType employeeType;
+
+
+        public void PerformWork()
+        {
+            numOfHoursWorked++;
+
+            Console.WriteLine($"{firstName} {lastName} is now working!");
+
+        }
+
+        public void StopWorking()
+        {
+            Console.WriteLine($"{firstName} {lastName} has stopped working!");
+        }
+
+        public double ReceiveWage()
+        {
+            wage = numOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"The wage for {numOfHoursWorked} hours of work is {wage}.");
+            numOfHoursWorked = 0;
+
+            return wage;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: {firstName}\nLast name: {lastName}\nEmail: {email}\nBirthday: {bday}\nEmployee Type: {employeeType}");
+
+        }
+
+
+
+
+    }
 }
+        
+
+
+
+    
+
+       
 
